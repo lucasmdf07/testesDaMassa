965259cf7bb09148281e9defac360080b8e375b2
lucasmdf07 <38864269+lucasmdf07@users.noreply.github.com>
2019-10-18 01:11:18 -0600

    massaDaMassaDaMassaTop

--- web/midwestOutdoors/index.php
+++ web/midwestOutdoors/index.php
@@ -22,14 +22,19 @@
 	<?php
 	include 'database/db_access.php';
 		
-	foreach ($db->query("SELECT * FROM state") as $state_row) {
-		echo $state_row["name"] . ", ";
-		$stateId = $state_row["id"];
+
 			
 		foreach ($db->query("SELECT * FROM city WHERE state_id=$stateId") as $county_row) {
 			echo $county_row["name"];
+
+			foreach ($db->query("SELECT * FROM state") as $state_row) {
+				echo $state_row["name"] . ", ";
+				$stateId = $state_row["id"];
+
 			echo "<ul>";
 			$countyId = $county_row["id"];
+
+			
 
 			
 			foreach ($db->query("SELECT * FROM hotel WHERE city_id=$countyId") as $site_row) {
